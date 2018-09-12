3b36b0bcbb4559fbe9cd7c3b41b7da9daaeeaeb9
Killian Poulaud <killian.1024@gmail.com>
2018-09-12 21:16:24 +0200

    1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,8 @@ int main(int argc, char* argv[])
     ap.add_help_arg({"--help"}, "Display this help and exit.");
     ap.add_gplv3_version_arg({"--version"}, "Output version information and exit", "1.0.0", "2018",
                              "Killian Poulaud");
-    ap.add_foreign_arg("SOURCE-DIR", "Source directory", "", {spdap::avt_t::RWX_DIR});
-    ap.add_foreign_arg("DESTINATION-DIR", "Destination directory", "", {spdap::avt_t::RWX_DIR});
+    ap.add_keyless_arg("SOURCE-DIR", "Source directory", "", {spdap::avt_t::RWX_DIR});
+    ap.add_keyless_arg("DESTINATION-DIR", "Destination directory", "", {spdap::avt_t::RWX_DIR});
     ap.parse_args((unsigned int)argc, argv);
     
     classifier::program prog(
